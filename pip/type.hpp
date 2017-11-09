d3ed9d2774ebe759676dca6bce99de048b0442c7
samgoodrick <sdg31@zips.uakron.edu>
2017-11-08 19:40:06 -0500

    added new types

--- pip/type.hpp
+++ pip/type.hpp
@@ -10,8 +10,11 @@ namespace pip
     tk_int,
     tk_range,
     tk_wild,
+    tk_miss,
+    tk_port,
+    tk_loc,
   };
-
+  
   /// Represents a type.
   struct type : cc::node
   {
@@ -19,7 +22,7 @@ namespace pip
       : cc::node(k, {}) 
     { }
   };
-
+  
   /// The type of integer values. 
   ///
   /// TODO: I think we also need to represent integer encoding.
@@ -29,51 +32,76 @@ namespace pip
       : type(tk_int), width(w) 
     { }
     
-    /// The width (precision) of integer values.
+  /// The width (precision) of integer values.
     int width;
   };
-
+  
   /// A range over an integer type.
   struct range_type : type
   {
     range_type(type* t)
       : type(tk_range), value(t) 
     { }
-
-    /// The underlying integer type.
+    
+  /// The underlying integer type.
     type* value;
   };
-
+  
   /// The type of wildcard values.
   struct wild_type : type
   {
     wild_type(int w)
       : type(tk_wild), width(w) 
     { }
-  
-    /// The width (precision) of wildcard values.
+    
+  /// The width (precision) of wildcard values.
     int width;
-  };  
-
+  };
+  
+  /// The type of a miss expression; this is temporary.
+  /// miss expressions may change to have the type of
+  /// the key that was missed, or unit type
+  struct miss_type : type
+  {
+    miss_type()
+      : type(tk_miss)
+    {}
+  };
+  
+  /// The type of a port expression
+  struct port_type : type
+  {
+    port_type()
+      : type(tk_port)
+    {}
+  };
+  
+  /// The type of a location or field
+  struct loc_type : type
+  {
+    loc_type()
+      : type(tk_loc)
+    {}
+  };
+  
 // -------------------------------------------------------------------------- //
 // Operations
-
+  
   /// Returns the name of a type node.
   const char* get_node_name(type_kind k);
-
+  
   // Returns the kind of a type.
   inline type_kind
   get_type_kind(const type* t)
   {
     return static_cast<type_kind>(t->kind);
   }
-
-
+  
+  
   /// Returns the node name of a program.
   inline const char* 
   get_node_name(const type* t)
   {
     return get_node_name(get_type_kind(t));
   }
-
 } // namespace pip
